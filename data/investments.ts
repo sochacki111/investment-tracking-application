828c9da57795aa5c381598ce31a4f42026b94126
Michal Sochacki <michalsochacki111@gmail.com>
2024-10-29 10:07:44 +0100

    update types

--- data/investments.ts
+++ data/investments.ts
@@ -2,18 +2,19 @@ import 'server-only';
 import { db } from "@/db/drizzle";
 import { investment } from '@/db/schema';
 
+interface InvestmentToInsert {
+  name: string;
+  quantity: number;
+  buyPrice: number;
+  currentPrice: number;
+}
+
 export async function getInvestments() {
   const investments = await db.select().from(investment);
   return investments;
 }
 
-export async function addInvestment(investmentToInsert: {
-  name: string;
-  quantity: number;
-  buyPrice: number;
-  currentPrice: number;
-}) {
-  console.log(investmentToInsert);
+export async function addInvestment(investmentToInsert: InvestmentToInsert) {
   await db.insert(investment).values({
     name: investmentToInsert.name,
     quantity: investmentToInsert.quantity,
